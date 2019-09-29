2ff1a682603dde6347dccc96f705758fb3f1742b
ytyaru <ytyaru@outlook.jp>
2019-09-29 09:49:42 +0900

    初回コミット

---
+++ src/run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import Precaution
+p = Precaution.Precaution()
+print(p.get_conjugations('動く'))
+print(p.get_conj_word('動く', '未然形'))
+print(p.get_conj_words('動く'))
+
